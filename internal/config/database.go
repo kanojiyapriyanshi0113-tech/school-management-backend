fbf36e671a1539cff6cb2afd6a9556ad331be51a
kanojiyapriyanshi01-web <kanojiyapriyanshi@gmail.com>
2026-06-30 13:25:13 +0530

    Add DB_DSN support

--- internal/config/database.go
+++ internal/config/database.go
@@ -9,14 +9,17 @@ import (
 )
 var DB *gorm.DB
 func ConnectDB() {
-    dsn := fmt.Sprintf(
-        "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Kolkata",
-        os.Getenv("DB_HOST"),
-        os.Getenv("DB_PORT"),
-        os.Getenv("DB_USER"),
-        os.Getenv("DB_PASSWORD"),
-        os.Getenv("DB_NAME"),
-    )
+    dsn := os.Getenv("DB_DSN")
+    if dsn == "" {
+        dsn = fmt.Sprintf(
+            "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Kolkata",
+            os.Getenv("DB_HOST"),
+            os.Getenv("DB_PORT"),
+            os.Getenv("DB_USER"),
+            os.Getenv("DB_PASSWORD"),
+            os.Getenv("DB_NAME"),
+        )
+    }
     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
         log.Fatal("Failed to connect database:", err)
